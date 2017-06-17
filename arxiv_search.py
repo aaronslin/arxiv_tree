6923ba9cca4dcdc82f38645dba0b7431b32cbd37
Aaron Lin <lin.aaron1@gmail.com>
2017-06-17 13:00:28 -0400

    using pypdf for search instead

--- arxiv_search.py
+++ arxiv_search.py
@@ -1,40 +1,24 @@
-from cStringIO import StringIO
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-import os
-import sys, getopt
 import re
+import pyPdf
 
 doc_fname="test_docs/1706.03762.pdf"
-
-#converts pdf, returns its text content as a string
-def convert(fname, pages=None):
-    if not pages:
-        pagenums = set()
-    else:
-        pagenums = set(pages)
-
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    infile = file(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close
-    return text 
+text1 = "arXiv:1308.0850, 2013."
 
 #print convert(doc_fname)
 
-def all_arxiv_matches(text):
-    pattern = r"(?i)(arxiv:[0-9]{4}\.[0-9]{4,5}v?[0-9]{0,2})"
-    return re.findall(pattern, text)
+def pdf_to_text(filename):
+	PageFound = -1
+	pdfDoc = pyPdf.PdfFileReader(file(filename, "rb"))
+	content = ""
+	for i in range(0, pdfDoc.getNumPages()):
+		content += pdfDoc.getPage(i).extractText().encode("ascii", "ignore")
+		content += "\n"
+	return content
 
-text1 = "arXiv:1308.0850, 2013."
-print all_arxiv_matches(text1)
+def all_arxiv_matches(text):
+	pattern = r"(?i)(arxiv:[0-9]{4}\.[0-9]{4,5}v?[0-9]{0,2})"
+	return re.findall(pattern, text)
+
+def get_arxiv_citations(filename):
+	content = pdf_to_text(filename)
+	return all_arxiv_matches(content)
